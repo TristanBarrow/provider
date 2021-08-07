117dd895267f4159fcd0cffd47b98d2927032062
Tristan Barrow <tristanmarkbarrow@gmail.com>
2021-08-06 22:32:10 -0600

    added test federation component

---
+++ components/FedComp.tsx
@@ -0,0 +1,7 @@
+const FedComp = () => {
+    return (
+        <div>A different thing.</div>
+    )
+}
+
+export default FedComp;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  reactStrictMode: true,
-}
+// next.config.js
+const { withFederatedSidecar } = require("@module-federation/nextjs-mf");
+
+module.exports = withFederatedSidecar({
+  name: "wmProvider",
+  filename: "static/chunks/remoteEntry.js",
+  exposes: {
+    "./FedComp": "./components/FedComp.tsx",
+  },
+  shared: {
+    react: {
+      // Notice shared are NOT eager here.
+      requiredVersion: false,
+      singleton: true,
+    },
+  },
+})({
+  // your original next.config.js export
+});
+
+// module.exports = {
+//   reactStrictMode: true,
+// }
